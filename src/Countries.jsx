b87fd64bc9819b03c2d70695ef69f0c462e235ec
Mousamia <87888309+Mousamia@users.noreply.github.com>
2024-03-07 23:04:48 +0600

    hfrkgjg

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -13,15 +13,30 @@ const Countries = () => {
             .then(res => res.json())
             .then(data => setCountries(data))
     }, [])
+
+    const [visitedCountries, setVisitedCountries] = useState([]);
+
+    const handleClickVisited = (country) => {
+        console.log(country);
+        const updatedCountries = [ ...visitedCountries, country ];
+        console.log(updatedCountries);
+        setVisitedCountries(updatedCountries);
+        console.log(updatedCountries);
+    }
     return (
         <div>
             <h2 className="text-3xl"> Countries will appear here</h2>
+            <p>Your Visited countries will appear here</p>
             <h3>{countries.length}</h3>
+            {
+                visitedCountries.map (visitedCountry => <li>{visitedCountry.name}</li> )
+            }
+            
             <div className="grid grid-cols-3">
             {
                 countries.map(country =>
                     <Country
-                        country={country}>
+                    handleClickVisited = {handleClickVisited}   country={country}>
                     </Country>)
             }
             </div>
--- src/Country.jsx
+++ src/Country.jsx
@@ -2,20 +2,22 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 
-const Country = ({ country }) => {
-    console.log(country);
+const Country = ({ country , handleClickVisited}) => {
+    // console.log(country);
+    // console.log(handleClickVisited);
     const [visited, setVisited] = useState(false);
+    
     const checkVisited = () => {
         setVisited(!visited);
 
     }
     return (
-        <div className={`  ${visited ? 'border-4 border-sky-400 m-3' : 'bg-purple-900'}`}>
+        <div className={`   ${visited ? 'border-4 border-sky-400 m-3' : 'border-4 border-yellow-400 m-3'}`}>
             <h3 className="text 2xl">{country.name} </h3>
             <h3 className="text 2xl">Area: {country.area} </h3>
             <img src={country.flag} className='w-40 h-20' alt="" />
             <button onClick={checkVisited} className="p-4 m-3 font-bold bg-blue-700">Mark as visited</button>
-            {/* <button className="p-4 m-3 font-bold bg-green-700">Visited</button> */}
+            <button onClick={() => handleClickVisited(country)}  className="p-4 m-3 font-bold bg-green-700">Visited</button>
             {
                 visited ? "Im Visited" : "Not visited"
             }
